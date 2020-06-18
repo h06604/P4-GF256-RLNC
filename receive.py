9b0d5b1b5889effced6dbc7fa3947ed958ddf6eb
h06604 <a0989659402@gmail.com>
2020-06-18 15:45:26 +0800

    second commit

---
+++ receive.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import struct
+import os
+
+from scapy.all import sniff, sendp, hexdump, get_if_list, get_if_hwaddr
+from scapy.all import Packet, IPOption
+from scapy.all import ShortField, IntField, LongField, BitField, FieldListField, FieldLenField
+from scapy.all import IP, TCP, UDP, Raw
+from scapy.layers.inet import _IPOption_HDR
+
+def get_if():
+    ifs=get_if_list()
+    iface=None
+    for i in get_if_list():
+        if "wlo1" in i:
+            iface=i
+            break;
+    if not iface:
+        print "Cannot find eth0 interface"
+        exit(1)
+    return iface
+
+def handle_pkt(pkt):
+    if TCP in pkt and pkt[TCP].dport == 1234:
+        print "got a packet"
+        pkt.show2()
+    #    hexdump(pkt)
+        sys.stdout.flush()
+
+
+def main():
+
+    iface = get_if()
+    print "sniffing on %s" % iface
+    sys.stdout.flush()
+    sniff(iface = iface,
+          prn = lambda x: handle_pkt(x))
+
+if __name__ == '__main__':
+    main()
